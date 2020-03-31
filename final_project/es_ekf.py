55a754b754741c8a2891351132686074c4539a8a
AMIN <modamine@gmail.com>
2020-03-31 23:28:43 +0100

    finished part1 and part2

--- final_project/es_ekf.py
+++ final_project/es_ekf.py
@@ -18,7 +18,7 @@ print('am here')
 # This is where you will load the data from the pickle files. For parts 1 and 2, you will use
 # p1_data.pkl. For Part 3, you will use pt3_data.pkl.
 ################################################################################################
-with open('/home/modamine/Desktop/state_estimation/final_project/data/pt1_data.pkl', 'rb') as file:
+with open('/home/modamine/Desktop/state_estimation/final_project/data/pt3_data.pkl', 'rb') as file:
     data = pickle.load(file)
 print('am here')
 ################################################################################################
@@ -81,12 +81,12 @@ C_li = np.array([
    [-0.04998,  0.04992,  0.9975 ]
 ])
 
-# Incorrect calibration rotation matrix, corresponding to Euler RPY angles (0.05, 0.05, 0.05).
-# C_li = np.array([
+#Incorrect calibration rotation matrix, corresponding to Euler RPY angles (0.05, 0.05, 0.05).
+#C_li = np.array([
 #      [ 0.9975 , -0.04742,  0.05235],
 #      [ 0.04992,  0.99763, -0.04742],
 #      [-0.04998,  0.04992,  0.9975 ]
-# ])
+#])
 
 t_i_li = np.array([0.5, 0.1, 0.5])
 
@@ -100,10 +100,10 @@ lidar.data = (C_li @ lidar.data.T).T + t_i_li
 # most important aspects of a filter is setting the estimated sensor variances correctly.
 # We set the values here.
 ################################################################################################
-var_imu_f = 0.10
+var_imu_f = 0.5
 var_imu_w = 0.25
-var_gnss  = 0.01
-var_lidar = 1.00
+var_gnss  = 0.2
+var_lidar = 5
 
 ################################################################################################
 # We can also set up some constants that won't change for any iteration of our solver.
@@ -145,16 +145,16 @@ def measurement_update(sensor_var, p_cov_check, y_k, p_check, v_check, q_check):
         [0,1,0,0,0,0,0,0,0],
         [0,0,1,0,0,0,0,0,0]
     ])
-    K_k = p_cov_check.T * H_k.T * inv(H_k*p_cov_check.T*H_k.T + sensor_var)
+    K_k = p_cov_check @ H_k.T @ inv(H_k@p_cov_check@H_k.T + sensor_var)
     # 3.2 Compute error state
-    error_state = (K_k * (y_k - p_check).T).T
+    error_state = (K_k @ (y_k - p_check).T).T
+    slice = np.array([error_state[0,6],error_state[0,7],error_state[0,8]])
     # 3.3 Correct predicted state
-    p_hat = p_check + error_state[0:3]
-    v_hat = v_check + error_state[3:6]
-    q_theta = Quaternion(euler = error_state[6:9])
-    q_hat = q_theta.quat_mult_right(q_check)
+    p_hat = p_check + error_state[0,0:3]
+    v_hat = v_check + error_state[0,3:6]
+    q_hat = Quaternion(euler= slice).quat_mult_left(q_check)
     # 3.4 Compute corrected covariance
-    p_cov_hat = (np.identity(9)-K_k * H_k)* p_cov_check
+    p_cov_hat = (np.identity(9)-K_k @ H_k)@ p_cov_check
 
 
     return p_hat, v_hat, q_hat, p_cov_hat
@@ -167,22 +167,29 @@ print('am here')
 ################################################################################################
 for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial prediction from gt
     delta_t = imu_f.t[k] - imu_f.t[k - 1]
-    mata = np.mat([imu_f.data[k-1].T])
-    print(mata.T)
+    
+    
+   
     # 1. Update state with IMU inputs
     C_ns = Quaternion(*q_est[k-1]).to_mat()
-    p_check = p_est[k-1] + delta_t * v_est[k-1] + [(delta_t*delta_t * (C_ns * mata.T + g.T)/2)]
-    v_check = v_est[k-1] + delta_t*(C_ns * mata.T + g.T).T
-    q_t = Quaternion(axis_angle= imu_w.data[k-1]* delta_t)
-    q_check = q_t.quat_mult_right(q_est[k-1])
+    mata = np.mat([imu_f.data[k-1]])
+
+    p_check = p_est[k-1] + delta_t * v_est[k-1] + ((np.square(delta_t)*0.5) * ((C_ns @ mata.T + g.T))).T
+
+    v_check = v_est[k-1] + (delta_t*(C_ns @ mata.T + g.T).T)
+    
+    q_check = Quaternion(axis_angle= delta_t * imu_w.data[k-1]).quat_mult_right(q_est[k-1])
+
+    
 
     # 1.1 Linearize the motion model and compute Jacobians
     
     F_k = np.block([
-        [np.identity(3),np.identity(3)*delta_t,np.zeros((3,3))],
-        [np.zeros((3,3)),np.identity(3),-C_ns * mata.T*delta_t*np.identity(3)],
+        [np.identity(3),delta_t*np.identity(3),np.zeros((3,3))],
+        [np.zeros((3,3)),np.identity(3),np.zeros((3,3))],
         [np.zeros((3,3)),np.zeros((3,3)),np.identity(3)]
     ])
+    F_k[3:6,6:9] = -(skew_symmetric(C_ns@imu_f.data[k-1].reshape((3,1)))) * delta_t
     L_k = np.block([
         [np.zeros([3,3]),np.zeros([3,3])],[np.identity(3),np.zeros([3,3])],[np.zeros([3,3]),np.identity(3)]
     ])
@@ -193,12 +200,26 @@ for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial predic
     # 2. Propagate uncertainty
     p_cov_check = F_k @ p_cov[k-1] @ F_k.T  + L_k @ Q @ L_k.T
     # 3. Check availability of GNSS and LIDAR measurements
+    R_l = (var_lidar**2) * np.identity(3)
+    R_G = (var_gnss**2) * np.identity(3)
+    if  imu_f.t[k] >= gnss.t[gnss_i]  and gnss_i < 48 :
+        p_check,v_check,q_check,p_cov_check = measurement_update(R_G,p_cov_check,gnss.data[gnss_i],p_check[0],v_check[0],q_check)
+        gnss_i += 1
+        
+    if imu_f.t[k] >= lidar.t[lidar_i]  and lidar_i < 48 :
+        p_check,v_check,q_check,p_cov_check = measurement_update(R_l,p_cov_check,lidar.data[lidar_i],p_check[0],v_check[0],q_check)
+        lidar_i += 1
+        
+        
+        
 
+    #print(lidar.t[lidar_i])
     # Update states (save)
     p_est[k] = p_check
     v_est[k] = v_check
     q_est[k] = q_check
     p_cov[k] = p_cov_check
+    
     
 
 #### 6. Results and Analysis ###################################################################
@@ -219,7 +240,7 @@ ax.set_zlabel('Up [m]')
 ax.set_title('Ground Truth and Estimated Trajectory')
 ax.set_xlim(0, 200)
 ax.set_ylim(0, 200)
-ax.set_zlim(-2, 2)
+ax.set_zlim(-10, 10)
 ax.set_xticks([0, 50, 100, 150, 200])
 ax.set_yticks([0, 50, 100, 150, 200])
 ax.set_zticks([-2, -1, 0, 1, 2])
@@ -284,7 +305,7 @@ p1_str = ''
 for val in p1_indices:
     for i in range(3):
         p1_str += '%.3f ' % (p_est[val, i])
-with open('pt1_submission.txt', 'w') as file:
+with open('pt3_submission.txt', 'w') as file:
     file.write(p1_str)
 
 # Pt. 2 submission
